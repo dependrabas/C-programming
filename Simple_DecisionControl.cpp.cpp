4bb19f604eee1fb3ea198803281c4ae03bb07460
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-13 22:50:44 +0530

    Add files via upload
    Simple Decision control statement in c++

---
+++ Simple_DecisionControl.cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Age;
+    cout<<"Enter your Age:";
+    cin>>Age;
+    switch (Age) {
+        caseAge>=16:
+            cout << "Too young";
+            break;
+        case Age>=42&&Age<=16:
+            cout << "Adult";
+            break;
+        case Age<=70:
+            cout << "Senior";
+            break;
+    }
+    
+
+    return 0;
+}
