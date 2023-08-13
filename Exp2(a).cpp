604cf8cbc3c0a684561f869abd0b01f6a170a19c
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-13 16:27:52 +0530

    Add files via upload
    Basic C++ program

---
+++ Exp2(a).cpp
@@ -0,0 +1,18 @@
+//WAP to print the fibonacci Series till N number.
+#include<iostream>
+using namespace std;
+int main(){
+	int A ,B, C;
+	int n;
+	cout<<"Enter a Number you to see the Fibonnacci series:";
+	cin>>n;
+	C = A + B; 
+	while{
+	
+	
+	
+	}
+cout<<"The Value of C"<<C;
+
+	return 0;
+}
