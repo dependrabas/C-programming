1433c474b9fcf88d551b25f292c44d760ac508fa
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-17 23:19:56 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,2 @@
+const pi = 3.142;
+document.getElementById().innerHTML = "Hello World";
---
+++ login.js
@@ -0,0 +1,47 @@
+function $(id) {
+    return document.getElementById(id);
+}
+function ch() {
+    $("2").style.display = "none";
+    $("1").style.display = "inline";
+}
+
+function uc() {
+    $("1").style.display = "none";
+    $("2").style.display = "inline";
+}
+function login() {
+    var x = $("un").value;
+    var y = $("psw").value;
+
+    if (x.trim() != "" && y.trim() != "") {
+        alert("welcome back user : " + x);
+        $("info").innerHTML = "You are logged in as:<br><br>Username: <b>" + x + "</b><br>Password: <b>" + y + "</b>";
+        $("ma").style.display = "none";
+        $("out").style.display = "inline";
+    } else {
+        alert("Invalid user name or password")
+    }
+}
+function logout() {
+    $("un").value = "";
+    $("psw").value = "";
+    $("info").innerHTML = "";
+    $("out").style.display = "none";
+    $("ma").style.display = "inline";
+}
+
+// Edit on August 2020 
+window.onload = function () {
+    var inps = document.getElementsByTagName("input");
+    for (let ip of inps) {
+        ip.onfocus =
+            () => {
+                $("footer").style.display = "none";
+            }
+        ip.onblur =
+            () => {
+                $("footer").style.display = "block";
+            }
+    }
+}
