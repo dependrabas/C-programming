c3be870740ae6f95793ee34af014c2acd6f09bf3
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-20 14:36:37 +0530

    Add files via upload

---
+++ udemyLearning.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void logical_operators(int age, bool parental_consent, bool ssn, bool accidents) {
+    
+    //----WRITE YOUR CODE BELOW----
+    
+    if ((age >= 18 || (age >= 16 && parental_consent)) && ssn && !accidents)
+        cout << "Yes, you can work.";
+    
+    // Note using age > 15 in the above condition would better match the spec. Thanks Vikram!
+    
+    //----WRITE YOUR CODE ABOVE----
+}
