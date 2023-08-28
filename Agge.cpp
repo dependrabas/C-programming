6b2783ed11fceb93ca6ffbc9a56061c80ce42912
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-28 09:04:25 +0530

    Add files via upload
    simple program

---
+++ Agge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void can_you_drive(int age) {
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    
+    const int driving_age{16};
+    
+    if (age >= driving_age)
+        cout << "Yes - you can drive!";
+        
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+}
